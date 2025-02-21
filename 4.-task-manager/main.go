e7993f102b5c3e7031eb3a3a37c815640ab28dbb
anderson2093 <andersoncusma20@gmail.com>
2025-02-21 00:31:20 -0500

    main config

--- 4.-task-manager/main.go
+++ 4.-task-manager/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/anderson2093/ejercicios-go/4.-task-manager/config"
+	"github.com/anderson2093/ejercicios-go/4.-task-manager/db"
 )
 
 func main() {
+	config.ConnectDB()
+	db.InitTables()
+
 	fmt.Println("Servidor en ejecucion http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
