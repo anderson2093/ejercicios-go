ffe9264d7bde1cfaf5b2de2cd9cfd0b37c701dd8
anderson2093 <andersoncusma20@gmail.com>
2025-02-21 00:30:58 -0500

    metodos de task

--- 4.-task-manager/models/task.go
+++ 4.-task-manager/models/task.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/anderson2093/ejercicios-go/4.-task-manager/config"
+)
 
 type Task struct {
 	ID          int       `json:"id"`
@@ -8,4 +12,28 @@ type Task struct {
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
 	Completed   bool      `json:"completed"`
+}
+
+func GetTasks() ([]Task, error) {
+	rows, err := config.DB.Query("SELECT id, title,description ,completed, created_at FROM tasks")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		var task Task
+		if err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.Completed, &task.CreatedAt); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
+func CreateTask(title string, description string) error {
+	_, err := config.DB.Exec("INSERT INTO tasks (title,description,completed, created_at) VALUES ($1, $2, $3,$4)", title, description, false, time.Now())
+	return err
 }
